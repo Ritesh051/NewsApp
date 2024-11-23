38e50393423040c6333dfbce79b110ab836d2ed5
Ritesh Singh <142240934+Ritesh051@users.noreply.github.com>
2024-11-23 17:27:37 +0000

    proxy change

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Navbar from './Components/Navbar';
 import News from './Components/News';
-import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import LoadingBar from 'react-top-loading-bar';
 import Footer from './Components/Footer';
 import NotFound from './Components/NotFound';
@@ -18,31 +18,29 @@ const App = () => {
 
   return (
     <>
-      <HashRouter>
-        <LoadingBar
-          color='#f11946'
-          progress={progress}
-          onLoaderFinished={() => setProgress(0)}
-        />
-        <Navbar />
-        <div>
-          <Routes>
-            <Route path='/' element={<News setProgress={setProgress} apikey={apikey} pageSize={9} category="general" />} />
-            <Route path='/foryou' element={<News setProgress={setProgress} apikey={apikey} pageSize={9} category="general" />} />
-            <Route path='/business' element={<News setProgress={setProgress} apikey={apikey} pageSize={9} category="business" />} />
-            <Route path='/entertainment' element={<News setProgress={setProgress} apikey={apikey} pageSize={9} category="entertainment" />} />
-            <Route path='/general' element={<News setProgress={setProgress} apikey={apikey} pageSize={9} category="general" />} />
-            <Route path='/health' element={<News setProgress={setProgress} apikey={apikey} pageSize={9} category="health" />} />
-            <Route path='/science' element={<News setProgress={setProgress} apikey={apikey} pageSize={9} category="science" />} />
-            <Route path='/sports' element={<News setProgress={setProgress} apikey={apikey} pageSize={9} category="sports" />} />
-            <Route path='/technology' element={<News setProgress={setProgress} apikey={apikey} pageSize={9} category="technology" />} />
-            <Route path='/privacy' element={<Privacy />} />
-            <Route path='/terms' element={<Terms />} />
-            <Route path='*' element={<NotFound />} />
-          </Routes>
-        </div>
-        <Footer />
-      </HashRouter>
+      <LoadingBar
+        color='#f11946'
+        progress={progress}
+        onLoaderFinished={() => setProgress(0)}
+      />
+      <Navbar />
+      <div>
+        <Routes>
+          <Route path='/' element={<News setProgress={setProgress} apikey={apikey} pageSize={9} category="general" />} />
+          <Route path='/foryou' element={<News setProgress={setProgress} apikey={apikey} pageSize={9} category="general" />} />
+          <Route path='/business' element={<News setProgress={setProgress} apikey={apikey} pageSize={9} category="business" />} />
+          <Route path='/entertainment' element={<News setProgress={setProgress} apikey={apikey} pageSize={9} category="entertainment" />} />
+          <Route path='/general' element={<News setProgress={setProgress} apikey={apikey} pageSize={9} category="general" />} />
+          <Route path='/health' element={<News setProgress={setProgress} apikey={apikey} pageSize={9} category="health" />} />
+          <Route path='/science' element={<News setProgress={setProgress} apikey={apikey} pageSize={9} category="science" />} />
+          <Route path='/sports' element={<News setProgress={setProgress} apikey={apikey} pageSize={9} category="sports" />} />
+          <Route path='/technology' element={<News setProgress={setProgress} apikey={apikey} pageSize={9} category="technology" />} />
+          <Route path='/privacy' element={<Privacy />} />
+          <Route path='/terms' element={<Terms />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </div>
+      <Footer />
     </>
   );
 };
